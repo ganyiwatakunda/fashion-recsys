cb350918f321407fc3f0a9599abc1d82054f02e9
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2023-07-10 15:38:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
 from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
 
-features_list = pickle.load(open("embeddings.pkl", "rb"))
+features_list = pickle.load(open("embeddings.pkl", "rb" "buffering=0"),( encoding='bytes'))
 filenames = pickle.load(open('filenames.pkl','rb'))
 
 model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
